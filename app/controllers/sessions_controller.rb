8dee342ab1cf824590d3cddf36f80f98b91bdbe2
rahimshahad <rahim.shahad2@gmail.com>
2021-08-02 07:46:50 -0400

    commit tree 7f4e047b3283e1a06177b18059156c8e6cc5e6e1

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create 
+    if !params[:name] || params[:name].empty?
+			redirect_to(controller: 'sessions', action: 'new')
+		else
+			session[:name] = params[:name]
+			redirect_to(controller: 'application', action: 'hello')
+		end
+  end
+
+  def destroy
+		if session[:name]
+			session.delete :name
+		end
+		redirect_to(controller: 'sessions', action: 'new')
+	end
+
+end
